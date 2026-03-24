aac008222d03ffe0b86edcb734d1e37385ca801e
Dilley Andrade <dilleyandrade@gmail.com>
2026-03-23 22:13:21 -0300

    fix:ConfigLoader updated

--- src/main/scala/config/ConfigLoader.scala
+++ src/main/scala/config/ConfigLoader.scala
@@ -2,26 +2,18 @@ package config
 import com.typesafe.config.ConfigFactory
 
 
-case class DbConfig(
-                     pg_host: String,
-                     pg_port: String,
-                     pg_user: String,
-                     pg_password: String,
-                     pg_database: String,
-                     pg_driver: String
-                   )
-
+case class DbConfig( pg_host: String, pg_port: String, pg_user: String, pg_password: String, pg_database: String, pg_driver: String )
 
 object ConfigLoader {
 
   private val config = ConfigFactory.load()
 
   val pg_db: DbConfig = DbConfig(
-    config.getString("db_pg.pg_host"),
-    config.getString("db_pg.pg_port"),
-    config.getString("db_pg.pg_user"),
-    config.getString("db_pg.pg_password"),
-    config.getString("db_pg.pg_database"),
-    config.getString("db_pg.pg_driver"),
+    config.getString("db_pg.pg_host_conf"),
+    config.getString("db_pg.pg_port_conf"),
+    config.getString("db_pg.pg_user_conf"),
+    config.getString("db_pg.pg_password_conf"),
+    config.getString("db_pg.pg_database_conf"),
+    config.getString("db_pg.pg_driver_conf"),
   )
 }
